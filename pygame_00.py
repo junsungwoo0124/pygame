2a6758053b73536017197a25d6a8935745dcfe9c
NA <rahs0120@gmail.com>
2020-04-11 14:17:35 +0900

    pygame 프로젝트만들기

--- pygame_00.py
+++ pygame_00.py
@@ -6,5 +6,11 @@
     1. 'pygame' project 생성하기
     2. pygame module 설치하기
         2-1. file / setting / pygame interpreter
+    3. git과 연동하기
+        3-1. VCS / import into version control / share project on github
+        3-2 venv 가상환경 연동 해제하기
+    4.pygame_00.py 파일 commit
+    5. pygame_00.py 파일 git repository / push
+    6. github site 확인
 
 '''
