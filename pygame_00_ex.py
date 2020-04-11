2567fcbe82465204d267de09912a51a55b1ae9b2
NA <rahs0120@gmail.com>
2020-04-11 14:42:10 +0900

    pygame 윈도우 생성2

---
+++ pygame_00_ex.py
@@ -0,0 +1,11 @@
+# 800  x 800크기의 pygame 윈도우 만들기
+ import pygame
+ import time
+
+ pygame.init()
+ width = 800
+ heigh = 800
+ win_size = (width, heigh)
+ screen = pygame.display.set_mode(win_size)
+
+ time.sleep(2)
