d93fcd34b8652ebde4db09eb9b16793b9b6e1541
NA <rahs0120@gmail.com>
2020-04-11 14:46:21 +0900

    pygame 윈도우 안에 사각형 생성

--- pygame_00_ex2.py
+++ pygame_00_ex2.py
@@ -1,5 +1,4 @@
 # 1024 x  600크기의 창만들기
-
 import pygame
 import time
 
@@ -8,5 +7,8 @@ width = 1024
 heigh = 600
 win_size = (width,heigh)
 screen = pygame.display.set_mode(win_size)
+
+pygame.draw.rect(screen, (200,200,200), [10,10,500,50], 2)
+pygame.display.flip()
 
 time.sleep(2)
