6b25b2fb51a619d513587ee11fbf5604ed3d6149
NA <rahs0120@gmail.com>
2020-04-11 14:43:21 +0900

    pygame 내가 원하는 크기로 윈도우 생성

---
+++ pygame_00_ex2.py
@@ -0,0 +1,12 @@
+# 1024 x  600크기의 창만들기
+
+import pygame
+import time
+
+pygame.init()
+width = 1024
+heigh = 600
+win_size = (width,heigh)
+screen = pygame.display.set_mode(win_size)
+
+time.sleep(2)
